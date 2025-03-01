ce39e78ff8c426d375e5edcb820d09ec6233cd1e
sasha269209 <work269209@gmail.com>
2025-03-01 14:47:34 +0100

    Add new files

--- Arduino_001/src/main.cpp
+++ Arduino_001/src/main.cpp
@@ -3,13 +3,13 @@
 //
 
 const int PIN_A = 13;
-bool ON;
+bool ON = true;
 
 volatile unsigned long int timerLED1;
 unsigned long int timerRun;
 unsigned long int timerStart;
 
-ISR (TIMER0_COMPA_vect)
+ISR(TIMER0_COMPA_vect)
 {
   timerLED1++;
 }
@@ -18,12 +18,25 @@ void setup()
 {
   pinMode(PIN_A, OUTPUT);
   // interrapt settings
+
   TCCR0A |= (1 << WGM01);
-  // OCR0A = 0xF9; // 249
-  OCR0A = 0xC7; // 199 - C7
+
+  OCR0A = 0xF9; // 249
+  // OCR0A = 0xC7; // 199 - C7
+
   TIMSK0 |= (1 << OCIE0A);
-  // TCCR0B |= (1 << CS01) | (1 << CS00);
-  TCCR0B |= (1 << CS01);
+
+  // CS00=1 CS01=1 CS02=0 equal 64
+  // CS00=0 CS01=1 CS02=0 equal 8
+  TCCR0B |= (1 << CS00); // write 1 in CS00
+  // TCCR0B &= ~(1 << CS00);  // write 0 in CS00
+
+  TCCR0B |= (1 << CS01); // write 1 in CS01
+  // TCCR0B &= ~(1 << CS01);  // write 0 in CS01
+
+  // TCCR0B |= (1 << CS02);   // write 1 in CS02
+  TCCR0B &= ~(1 << CS02); // write 0 in CS02
+
   sei();
 
   Serial.begin(9600);
@@ -35,14 +48,19 @@ void loop()
   timerRun = timerLED1;
   sei();
 
-  // delay (1000);
-  // Serial.println (timerRun);
-
-
-
-  if (timerRun - timerStart > 100)
+  if (timerRun - timerStart > 100 && ON)
   {
-    digitalWrite(PIN_A, ON = !ON);
+    digitalWrite(PIN_A, false);
+    ON = false;
     timerStart = timerRun;
+    Serial.println(timerRun);
+  }
+
+  if (timerRun - timerStart > 2000 && !ON)
+  {
+    digitalWrite(PIN_A, true);
+    ON = true;
+    timerStart = timerRun;
+    Serial.println(timerRun);
   }
 }
