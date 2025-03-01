e756ac8de2c91de4951a014ab71140545aaf67fc
sasha269209 <work269209@gmail.com>
2025-03-01 17:54:31 +0100

    Add new files

--- Arduino_001/src/main.cpp
+++ Arduino_001/src/main.cpp
@@ -19,23 +19,27 @@ void setup()
   pinMode(PIN_A, OUTPUT);
   // interrapt settings
 
-  TCCR0A |= (1 << WGM01);
+  cli();
+  TCCR0A = 0;
+  TCCR0B = 0;
 
-  OCR0A = 0xF9; // 249
-  // OCR0A = 0xC7; // 199 - C7
+  TCCR0A |= (1 << WGM01);
 
   TIMSK0 |= (1 << OCIE0A);
 
   // CS00=1 CS01=1 CS02=0 equal 64
   // CS00=0 CS01=1 CS02=0 equal 8
+  // CS00=1 CS01=0 CS02=0 equal 1
   TCCR0B |= (1 << CS00); // write 1 in CS00
   // TCCR0B &= ~(1 << CS00);  // write 0 in CS00
 
-  TCCR0B |= (1 << CS01); // write 1 in CS01
+  // TCCR0B |= (1 << CS01); // write 1 in CS01
   // TCCR0B &= ~(1 << CS01);  // write 0 in CS01
 
   // TCCR0B |= (1 << CS02);   // write 1 in CS02
-  TCCR0B &= ~(1 << CS02); // write 0 in CS02
+  // TCCR0B &= ~(1 << CS02); // write 0 in CS02
+
+  OCR0A = 7; // 7 = 0.5 mk cek
 
   sei();
 
@@ -48,7 +52,7 @@ void loop()
   timerRun = timerLED1;
   sei();
 
-  if (timerRun - timerStart > 100 && ON)
+  if (timerRun - timerStart > 2 && ON)
   {
     digitalWrite(PIN_A, false);
     ON = false;
@@ -56,7 +60,7 @@ void loop()
     Serial.println(timerRun);
   }
 
-  if (timerRun - timerStart > 2000 && !ON)
+  if (timerRun - timerStart > 10 && !ON)
   {
     digitalWrite(PIN_A, true);
     ON = true;
